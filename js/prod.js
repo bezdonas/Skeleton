c8f512f26d042d899fbe9dd24d13046d224439e2
Ramil Aliev <bezdonas@gmail.com>
2013-12-21 16:50:28 +0400

    Added server

--- js/prod.js
+++ js/prod.js
@@ -9656,26 +9656,5 @@ window.Modernizr = (function( window, document, undefined ) {
 Modernizr.load=function(){yepnope.apply(window,[].slice.call(arguments,0));};
 ;
 $(function() {
-	$('.js-pluginName').pluginName();
+
 });
-
-
-(function($) {
-
-	$.fn.pluginName = function(args) {
-
-		return this.each(function() {
-			var that = $(this),
-				conf = $.extend({}, $.fn.pluginName.defaults, args, that.data('pluginName'));
-			
-			console.log(conf.message);
-
-		});
-
-	};
-
-	$.fn.pluginName.defaults = {
-		message: 'plugin started'
-	};
-
-})(jQuery);
